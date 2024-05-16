254a9aac4a2a4f4f863ee9969451da7a9ee3823e
felix <felixCamposLu@gmail.com>
2024-05-15 20:51:38 -0500

    v5

---
+++ index.js
@@ -0,0 +1,13 @@
+const EXAMPLES = {
+    components: {
+        title: 'mac'
+    },
+    ajx: {
+        title: 'mmox',
+        nombre: 'max'
+    }
+}
+
+const components = EXAMPLES['ajx'].title;
+
+console.log(components);
